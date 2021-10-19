2b6108f8027266742a4932abd9af355a7d4fb8c1
Luiz Pansarini <lpansarini@gmail.com>
2021-10-18 21:03:43 -0300

    initial backend

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,60 @@
+import axios from 'axios'
+
+import prismaCLient from '../prisma'
+
+interface IAccessTokenResponse {
+    access_token: string;
+}
+
+interface IUserResponse {
+    avatar_url: string;
+    login: string;
+    id: number;
+    name: string;
+}
+
+class AuthenticateUserService {
+    async execute(code: string) {
+        const url = "https://github.com/login/oauth/access_token";
+
+        const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+            params: {
+                client_id: process.env.GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code,
+            },
+            headers: {
+                "Accept": "application/json"
+            }
+        })
+
+        const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+            headers: {
+                authorization: `Bearer ${accessTokenResponse.access_token}`
+            }
+        })
+
+        const { login, id, avatar_url, name } = response.data;
+
+        let user = await prismaCLient.user.findFirst({
+            where: {
+                github_id: id
+            }
+        })
+
+        if (!user) {
+            user = await prismaCLient.user.create({
+                data: {
+                    github_id: id,
+                    login,
+                    avatar_url,
+                    name
+                }
+            })
+        }
+
+        return response.data
+    }
+}
+
+export { AuthenticateUserService }
